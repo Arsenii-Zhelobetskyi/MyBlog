a1e1a4768e3bde7f145bb945139604d35c8d144d
Arsenii-Zhelobetskyi <arsbrotherplay@gmail.com>
2024-10-12 20:38:33 +0300

    added new commands

--- src/components/Editor/SlashCommand/items.jsx
+++ src/components/Editor/SlashCommand/items.jsx
@@ -1,19 +1,25 @@
+import {
+  Heading1,
+  Heading2,
+  Heading3,
+  Type,
+  List,
+  ListOrdered,
+  TextQuote,
+} from 'lucide-react';
 
-import { Heading1, Heading2, Heading3, Type } from 'lucide-react';
-
-const getSuggestionItems = ({query}) => {
+const getSuggestionItems = ({ query }) => {
   return [
     {
-
-      icon:<Type className="mr-2 h-4 w-4" />,
-      title: "Text",
+      icon: <Type className="mr-2 h-4 w-4" />,
+      title: 'Text',
       command: ({ editor, range }) => {
-        editor.chain().focus().deleteRange(range).setNode("paragraph").run();
-      }
+        editor.chain().focus().deleteRange(range).setNode('paragraph').run();
+      },
     },
     {
-      icon:<Heading1 className="mr-2 h-4 w-4" />,
-      title: "H1",
+      icon: <Heading1 className="mr-2 h-4 w-4" />,
+      title: 'H1',
       command: ({ editor, range }) => {
         editor
           .chain()
@@ -21,24 +27,23 @@ const getSuggestionItems = ({query}) => {
           .deleteRange(range)
           .toggleHeading({ level: 1 })
           .run();
-      }
+      },
     },
     {
-      icon:<Heading2 className="mr-2 h-4 w-4" />,
-      title: "H2",
+      icon: <Heading2 className="mr-2 h-4 w-4" />,
+      title: 'H2',
       command: ({ editor, range }) => {
         editor
-        .chain()
-        .focus()
+          .chain()
+          .focus()
           .deleteRange(range)
           .toggleHeading({ level: 2 })
           .run();
-      }
+      },
     },
     {
-
-      icon:<Heading3 className="mr-2 h-4 w-4" />,
-      title: "H3",
+      icon: <Heading3 className="mr-2 h-4 w-4" />,
+      title: 'H3',
       command: ({ editor, range }) => {
         editor
           .chain()
@@ -46,29 +51,38 @@ const getSuggestionItems = ({query}) => {
           .deleteRange(range)
           .toggleHeading({ level: 3 })
           .run();
-      }
+      },
     },
     {
-      title: "bold",
+      icon: <List className="mr-2 h-4 w-4" />,
+      title: 'Bullet list',
       command: ({ editor, range }) => {
-        editor.chain().focus().deleteRange(range).setMark("bold").run();
-      }
+        editor.chain().focus().deleteRange(range).toggleBulletList().run();
+      },
     },
     {
-      title: "italic",
+      icon: <ListOrdered className="mr-2 h-4 w-4" />,
+      title: 'Ordered list',
       command: ({ editor, range }) => {
-        editor.chain().focus().deleteRange(range).setMark("italic").run();
-      }
+        editor.chain().focus().deleteRange(range).toggleOrderedList().run();
+      },
     },
     {
-      title: "image",
+      icon: <TextQuote className="mr-2 h-4 w-4" />,
+      title: 'Blockquote',
       command: ({ editor, range }) => {
-        editor.chain().focus().deleteRange(range).setNode("paragraph").run();
-      }
-    }
+        editor.chain().focus().deleteRange(range).toggleBlockquote().run();
+      },
+    },
+    {
+      title: 'image',
+      command: ({ editor, range }) => {
+        editor.chain().focus().deleteRange(range).setNode('paragraph').run();
+      },
+    },
   ]
     .filter((item) => item.title.toLowerCase().startsWith(query.toLowerCase()))
     .slice(0, 10);
-  };
+};
 
-  export default getSuggestionItems;
+export default getSuggestionItems;
